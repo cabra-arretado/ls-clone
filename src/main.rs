f1894c84aa33a9ee694b258ec3a8b38deeb2acc1
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2023-09-22 11:09:30 -0500

    handle error

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::fs;
 
 fn main() {
-    list_files(".");
+    list_files(&String::from("."));
 }
 
 fn list_files(folder_path: &str) {
     let paths = fs::read_dir(folder_path).unwrap();
     for path in paths {
+        if path.is_err() {
+            continue;
+        }
+        //TODO: Remove the main path from the string
         println!("{}", path.unwrap().path().display());
     }
 }
