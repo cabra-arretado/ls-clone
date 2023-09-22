b6d887b486597c28e31a5ae98ac9755af161bda7
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2023-09-22 11:00:32 -0500

    accept folder path

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::fs;
 
 fn main() {
-    list_files();
+    list_files(".");
 }
 
-fn list_files() {
-    let paths = fs::read_dir("./").unwrap();
+fn list_files(folder_path: &str) {
+    let paths = fs::read_dir(folder_path).unwrap();
     for path in paths {
         println!("{}", path.unwrap().path().display());
     }
