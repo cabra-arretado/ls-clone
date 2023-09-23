844316a197ef9eeb21d08c763a14ff2901d1e0c1
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2023-09-22 22:34:14 -0500

    trying out getopt crate

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
+use getopts::Options;
+
 use std::env;
 use std::fs;
 
 fn main() {
     let args = read_args();
-    // dbg!(&args);
+    // let program = args[0].clone();
+    let mut opts = Options::new();
+    opts.optflag("a", "help", "print this help menu");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!("{}", f.to_string()) }
+    };
+    if matches.opt_present("h") {
+        println!("{}", String::from("Help me"));
+        return;
+    }
     list_files(&args[0]);
 }
 
@@ -17,13 +29,14 @@ fn list_files(folder_path: &str) {
                     continue;
                 }
                 println!("{}", path.unwrap().path().display());
+                std::process::exit(0);
             }
         }
     }
 }
 
 fn read_args() -> Vec<String> {
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<_> = env::args().collect();
     if args.len() < 2 {
         println!("Error: missing arguments");
         std::process::exit(1);
