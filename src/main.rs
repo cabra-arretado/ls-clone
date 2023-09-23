45e42cdc81a7c22543ab01970672ca0efd000338
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2023-09-22 19:33:37 -0500

    reads args

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::fs;
+use std::env;
 
 fn main() {
+    let args = read_args();
+    dbg!(args);
     list_files(&String::from("."));
 }
 
@@ -13,4 +16,9 @@ fn list_files(folder_path: &str) {
         //TODO: Remove the main path from the string
         println!("{}", path.unwrap().path().display());
     }
+}
+
+fn read_args() -> Vec<String> {
+    let args: Vec<String> = env::args().collect();
+    args
 }
