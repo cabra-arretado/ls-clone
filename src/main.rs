588ff9307aa4eb9cde7401db1ae5308c666349bf
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2023-09-22 22:02:34 -0500

    pass validation to args

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@ use std::fs;
 fn main() {
     let args = read_args();
     // dbg!(&args);
-    list_files(&args[1]);
+    list_files(&args[0]);
 }
 
 fn list_files(folder_path: &str) {
     let paths = fs::read_dir(folder_path);
     match paths {
-        Err(_) => println!("Error reading the path"),
+        Err(_) => println!("Error: reading the path"),
         Ok(_) => {
             for path in paths.unwrap() {
                 if path.is_err() {
@@ -24,5 +24,9 @@ fn list_files(folder_path: &str) {
 
 fn read_args() -> Vec<String> {
     let args: Vec<String> = env::args().collect();
-    args
+    if args.len() < 2 {
+        println!("Error: missing arguments");
+        std::process::exit(1);
+    }
+    args[1..].to_vec()
 }
