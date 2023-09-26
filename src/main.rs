90788df078b58beec53e9609b1afcaa2818b79c3
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2023-09-25 20:54:00 -0500

    clean code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use getopts::Options;
+extern crate getopts;
 
 use std::env;
 use std::fs;
@@ -6,33 +6,45 @@ use std::fs;
 fn main() {
     let args = read_args();
     // let program = args[0].clone();
-    let mut opts = Options::new();
-    opts.optflag("a", "help", "print this help menu");
-    let matches = match opts.parse(&args[1..]) {
-        Ok(m) => { m }
-        Err(f) => { panic!("{}", f.to_string()) }
-    };
-    if matches.opt_present("h") {
-        println!("{}", String::from("Help me"));
-        return;
-    }
+    // let mut opts = getopts::Options::new();
+    // opts.optflag("a", "help", "print this help menu");
+    // let matches = match opts.parse(&args[1..]) {
+    //     Ok(m) => { m }
+    //     Err(f) => { panic!("{}", f.to_string()) }
+    // };
+    // if matches.opt_present("h") {
+    //     println!("{}", String::from("Help me"));
+    //     return;
+    // }
     list_files(&args[0]);
 }
 
 fn list_files(folder_path: &str) {
+    let files = get_files_vec(folder_path);
+    for file in files {
+        print!("{} ", file);
+    }
+}
+
+fn get_files_vec(folder_path: &str) -> Vec<String> {
     let paths = fs::read_dir(folder_path);
     match paths {
-        Err(_) => println!("Error: reading the path"),
+        Err(_) => { 
+            println!("Error: reading the path");
+            std::process::exit(1);
+        },
         Ok(_) => {
+            let mut files: Vec<String> = Vec::new();
             for path in paths.unwrap() {
                 if path.is_err() {
                     continue;
                 }
-                println!("{}", path.unwrap().path().display());
-                std::process::exit(0);
+                files.push(path.unwrap().path().display().to_string());
             }
+            files
         }
     }
+
 }
 
 fn read_args() -> Vec<String> {
